c08a61b8937dc3baea4d1a86c51ba8f813483d1b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2017-03-16 21:37:17 +0100

    basic functionality of the editor

--- editor.js
+++ editor.js
@@ -2,53 +2,66 @@
 
 class Editor {
 
+    constructor() {
+        this.nextId = 1;
+    }
+
     get(id) {
         return document.getElementById(id);
     }
 
-    createDiv(value) {
+    createDiv(value = this.nextId++, style) {
         const gallery = document.getElementById('listGallery');
         const output = document.getElementById('output');
         const li = document.createElement('li');
         const div = document.createElement('div');
 
-        let style = output.style.cssText;
-
-        div.style.cssText = style; 
+        div.style.cssText = style || output.style.cssText;
+        div.setAttribute('id', value);
 
         li.appendChild(div);
         gallery.insertBefore(li, gallery.firstChild);
 
         li.insertAdjacentHTML('afterbegin', '<button class="close">x</button>');
+
+        localStorage.setItem(div.id, div.style.cssText);
     }
 
     closeDiv(e) {
         const src = e.target;
+        const div = src.nextSibling;
         const parent = src.parentNode;
 
         if (src.nodeName.toLowerCase() === "button") {
+            localStorage.removeItem(div.id);
             parent.parentNode.removeChild(parent);
         }
     }
 
     setValue(e) {
-        const output = this.get('output');
+        const style = this.get('output').style;
         let target = e.target;
-        let name = target.name;
         let value = parseInt(target.value) ? target.value + 'px' : target.value;
 
-        console.log(output.style.name);
+        style[target.name] = value;
+    }
 
-        output.style.name = value;
+    getLocal() {
+        let divs = localStorage;
+        for (let div in divs) {
+            this.createDiv(div.id, divs[div]);
+        }
     }
 };
 
 const editor = new Editor();
 
-const save = editor.get('save');
-const controls = editor.get('controls');
+const saveButton = editor.get('save');
+const controlInputs = editor.get('controls');
 const gallery = editor.get('listGallery');
 
-controls.addEventListener('input', (e) => { editor.setValue(e) }, false);
-save.addEventListener('click', editor.createDiv, false);
+controlInputs.addEventListener('input', (e) => { editor.setValue(e) }, false);
+saveButton.addEventListener('click', () => { editor.createDiv() }, false);
 gallery.addEventListener('click', editor.closeDiv, false);
+
+document.addEventListener('DOMContentLoaded', () => { editor.getLocal() }, false);
